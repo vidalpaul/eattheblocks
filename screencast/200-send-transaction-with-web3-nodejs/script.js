39bb2379b4c4e5e276b75e636b0fadec0535983a
vidalpaul <u1d4lp@gmail.com>
2021-12-22 13:41:43 -0300

    tested 3 ways to connect a node backend to Ethereum via web3.js

--- screencast/200-send-transaction-with-web3-nodejs/script.js
+++ screencast/200-send-transaction-with-web3-nodejs/script.js
@@ -1,91 +1,94 @@
 const Web3 = require('web3');
 const Provider = require('@truffle/hdwallet-provider');
 const MyContract = require('./build/contracts/MyContract.json');
-const address = '';
-const privateKey = '';
-const infuraUrl = ''; 
+const address = '0x4033e8AEB8DFE4A51661553070e002457DbABC29';
+const privateKey =
+   '8866e91519f0b243f5e4da06c425038d27b1e62f24f43d5fb296cb43c8639572';
+const infuraUrl =
+   'https://ropsten.infura.io/v3/dac6b55872f34b2fb12c842a26b346ba';
 
-//Hard way (web3#signTransaction() + web3#sendSignedTransaction())
 const init1 = async () => {
-  const web3 = new Web3(infuraUrl);
-  const networkId = await web3.eth.net.getId();
-  const myContract = new web3.eth.Contract(
-    MyContract.abi,
-    MyContract.networks[networkId].address
-  );
+   const web3 = new Web3(infuraUrl);
+   const networkId = await web3.eth.net.getId();
+   const myContract = new web3.eth.Contract(
+      MyContract.abi,
+      MyContract.networks[networkId].address
+   );
 
-  const tx = myContract.methods.setData(1);
-  const gas = await tx.estimateGas({from: address});
-  const gasPrice = await web3.eth.getGasPrice();
-  const data = tx.encodeABI();
-  const nonce = await web3.eth.getTransactionCount(address);
+   const tx = myContract.methods.setData(1);
+   const gas = await tx.estimateGas({ from: address });
+   const gasPrice = await web3.eth.getGasPrice();
+   const data = tx.encodeABI();
+   const nonce = await web3.eth.getTransactionCount(address);
 
-  const signedTx = await web3.eth.accounts.signTransaction(
-    {
-      to: myContract.options.address, 
+   const signedTx = await web3.eth.accounts.signTransaction(
+      {
+         to: myContract.options.address,
+         data,
+         gas,
+         gasPrice,
+         nonce,
+         chainId: networkId,
+      },
+      privateKey
+   );
+   console.log(`Old data value: ${await myContract.methods.data().call()}`);
+   const receipt = await web3.eth.sendSignedTransaction(
+      signedTx.rawTransaction
+   );
+   console.log(`Transaction hash: ${receipt.transactionHash}`);
+   console.log(`New data value: ${await myContract.methods.data().call()}`);
+};
+
+// init1();
+
+const init2 = async () => {
+   const web3 = new Web3(infuraUrl);
+   const networkId = await web3.eth.net.getId();
+   const myContract = new web3.eth.Contract(
+      MyContract.abi,
+      MyContract.networks[networkId].address
+   );
+   web3.eth.accounts.wallet.add(privateKey);
+
+   const tx = myContract.methods.setData(2);
+   const gas = await tx.estimateGas({ from: address });
+   const gasPrice = await web3.eth.getGasPrice();
+   const data = tx.encodeABI();
+   const nonce = await web3.eth.getTransactionCount(address);
+
+   const txData = {
+      from: address,
+      to: myContract.options.address,
       data,
       gas,
       gasPrice,
-      nonce, 
-      chainId: networkId
-    },
-    privateKey
-  );
-  console.log(`Old data value: ${await myContract.methods.data().call()}`);
-  const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-  console.log(`Transaction hash: ${receipt.transactionHash}`);
-  console.log(`New data value: ${await myContract.methods.data().call()}`);
-}
+      nonce,
+      chain: 'ropsten',
+      hardfork: 'london',
+   };
 
-//Slightly easier (web3#sendTransaction())
-const init2 = async () => {
-  const web3 = new Web3(infuraUrl);
-  const networkId = await web3.eth.net.getId();
-  const myContract = new web3.eth.Contract(
-    MyContract.abi,
-    MyContract.networks[networkId].address
-  );
-  web3.eth.accounts.wallet.add(privateKey);
+   console.log(`Old data value: ${await myContract.methods.data().call()}`);
+   const receipt = await web3.eth.sendTransaction(txData);
+   console.log(`Transaction hash: ${receipt.transactionHash}`);
+   console.log(`New data value: ${await myContract.methods.data().call()}`);
+};
 
-  const tx = myContract.methods.setData(2);
-  const gas = await tx.estimateGas({from: address});
-  const gasPrice = await web3.eth.getGasPrice();
-  const data = tx.encodeABI();
-  const nonce = await web3.eth.getTransactionCount(address);
-  const txData = {
-    from: address,
-    to: myContract.options.address,
-    data: data,
-    gas,
-    gasPrice,
-    nonce, 
-    chain: 'rinkeby', 
-    hardfork: 'istanbul'
-  };
+// init2();
 
-  console.log(`Old data value: ${await myContract.methods.data().call()}`);
-  const receipt = await web3.eth.sendTransaction(txData);
-  console.log(`Transaction hash: ${receipt.transactionHash}`);
-  console.log(`New data value: ${await myContract.methods.data().call()}`);
-}
-
-//Easy way (Web3 + @truffle/hdwallet-provider)
 const init3 = async () => {
-  const provider = new Provider(privateKey, 'https://rinkeby.infura.io/v3/74aa9a15e2524f6980edb8a377301f3c'); 
-  const web3 = new Web3(provider);
-  const networkId = await web3.eth.net.getId();
-  const myContract = new web3.eth.Contract(
-    MyContract.abi,
-    MyContract.networks[networkId].address
-  );
+   const provider = new Provider(privateKey, infuraUrl);
+   const web3 = new Web3(provider);
+   const networkId = await web3.eth.net.getId();
+   const myContract = new web3.eth.Contract(
+      MyContract.abi,
+      MyContract.networks[networkId].address
+   );
 
-  console.log(await myContract.methods.data().call());
-  console.log(`Old data value: ${await myContract.methods.data().call()}`);
-  const receipt = await myContract.methods.setData(3).send({ from: address });
-  console.log(`Transaction hash: ${receipt.transactionHash}`);
-  console.log(`New data value: ${await myContract.methods.data().call()}`);
-}
+   console.log(`Old data value: ${await myContract.methods.data().call()}`);
+   const receipt = await myContract.methods.setData(3).send({ from: address });
+   console.log(`Transaction hash: ${receipt.transactionHash}`);
+   console.log(`New data value: ${await myContract.methods.data().call()}`);
+};
 
 init3();
-
-
--- screencast/200-send-transaction-with-web3-nodejs/truffle-config.js
+++ screencast/200-send-transaction-with-web3-nodejs/truffle-config.js
@@ -1,8 +1,12 @@
 const Provider = require('@truffle/hdwallet-provider');
-const address = '0xAD8eD8FBA46BA6bE9120d0aEc43109A0543b69d3';
-const privateKey = 'd448c9224e0bcf3a6475fb54a34056fb02ef469d1f5f8520bc0f6e443bde68c4';
+const address = '0x4033e8AEB8DFE4A51661553070e002457DbABC29';
+const privateKey =
+   '8866e91519f0b243f5e4da06c425038d27b1e62f24f43d5fb296cb43c8639572';
 
-const provider = new Provider(privateKey, 'https://rinkeby.infura.io/v3/74aa9a15e2524f6980edb8a377301f3c'); 
+const provider = new Provider(
+   privateKey,
+   'https://ropsten.infura.io/v3/dac6b55872f34b2fb12c842a26b346ba'
+);
 
 /**
  * Use this file to configure your truffle project. It's seeded with some
@@ -31,75 +35,75 @@ const provider = new Provider(privateKey, 'https://rinkeby.infura.io/v3/74aa9a15
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
-  /**
-   * Networks define how you connect to your ethereum client and let you set the
-   * defaults web3 uses to send transactions. If you don't specify one truffle
-   * will spin up a development blockchain for you on port 9545 when you
-   * run `develop` or `test`. You can ask a truffle command to use a specific
-   * network from the command line, e.g
-   *
-   * $ truffle test --network <network-name>
-   */
+   /**
+    * Networks define how you connect to your ethereum client and let you set the
+    * defaults web3 uses to send transactions. If you don't specify one truffle
+    * will spin up a development blockchain for you on port 9545 when you
+    * run `develop` or `test`. You can ask a truffle command to use a specific
+    * network from the command line, e.g
+    *
+    * $ truffle test --network <network-name>
+    */
 
-  networks: {
-    // Useful for testing. The `development` name is special - truffle uses it by default
-    // if it's defined here and no other network is specified at the command line.
-    // You should run a client (like ganache-cli, geth or parity) in a separate terminal
-    // tab if you use this network and you must also set the `host`, `port` and `network_id`
-    // options below to some value.
-    //
-    // development: {
-    //  host: "127.0.0.1",     // Localhost (default: none)
-    //  port: 8545,            // Standard Ethereum port (default: none)
-    //  network_id: "*",       // Any network (default: none)
-    // },
+   networks: {
+      // Useful for testing. The `development` name is special - truffle uses it by default
+      // if it's defined here and no other network is specified at the command line.
+      // You should run a client (like ganache-cli, geth or parity) in a separate terminal
+      // tab if you use this network and you must also set the `host`, `port` and `network_id`
+      // options below to some value.
+      //
+      // development: {
+      //  host: "127.0.0.1",     // Localhost (default: none)
+      //  port: 8545,            // Standard Ethereum port (default: none)
+      //  network_id: "*",       // Any network (default: none)
+      // },
 
-    // Another network with more advanced options...
-    // advanced: {
+      // Another network with more advanced options...
+      // advanced: {
       // port: 8777,             // Custom port
       // network_id: 1342,       // Custom network
       // gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
       // gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
       // from: <address>,        // Account to send txs from (default: accounts[0])
       // websockets: true        // Enable EventEmitter interface for web3 (default: false)
-    // },
+      // },
 
-    // Useful for deploying to a public network.
-    // NB: It's important to wrap the provider as a function.
-     rinkeby: {
-       provider: () => provider,
-       network_id: 4,
-      // gas: 5500000,        // Ropsten has a lower block limit than mainnet
-      // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-     },
+      // Useful for deploying to a public network.
+      // NB: It's important to wrap the provider as a function.
+      ropsten: {
+         provider: () => provider,
+         network_id: 3,
+         // gas: 5500000,        // Ropsten has a lower block limit than mainnet
+         // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+         // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+         // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      },
 
-    // Useful for private networks
-    // private: {
+      // Useful for private networks
+      // private: {
       // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
       // network_id: 2111,   // This network is yours, in the cloud.
       // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
-  },
-
-  // Set default mocha options here, use special reporters etc.
-  mocha: {
-    // timeout: 100000
-  },
-
-  // Configure your compilers
-  compilers: {
-    solc: {
-      // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
       // }
-    }
-  }
-}
+   },
+
+   // Set default mocha options here, use special reporters etc.
+   mocha: {
+      // timeout: 100000
+   },
+
+   // Configure your compilers
+   compilers: {
+      solc: {
+         // version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
+         // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+         // settings: {          // See the solidity docs for advice about optimization and evmVersion
+         //  optimizer: {
+         //    enabled: false,
+         //    runs: 200
+         //  },
+         //  evmVersion: "byzantium"
+         // }
+      },
+   },
+};
